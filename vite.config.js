d8954f137b8911303d3a0c5df80abc430fd80f40
HowlerTheDad <107449419+HowlerTheDad@users.noreply.github.com>
2025-04-16 16:23:06 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/howlerthedad.github.io/React_ecommerce/',
   plugins: [react(), tailwindcss()],
 })
